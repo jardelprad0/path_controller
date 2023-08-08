6723953c490eec940f4bb1a9712ebc2bd65c7d61
JardelPrad0 <jardelprado.tec@gmail.com>
2023-08-08 17:39:45 -0300

    funcao_alinha

--- scripts/path_controller.py
+++ scripts/path_controller.py
@@ -14,7 +14,23 @@ from matplotlib import cm
 import time
 from environment import Env
 
-# ... (existing imports and code)
+def alinha_robo(heading):
+    # Calcular o erro de orientação usando o valor de heading do ambiente
+    orientation_error = heading
+
+    # Normalizar o erro de orientação para estar no intervalo [-pi, pi]
+    if orientation_error > math.pi:
+        orientation_error -= 2 * math.pi
+    elif orientation_error < -math.pi:
+        orientation_error += 2 * math.pi
+
+    # Calcular a velocidade angular com base no erro de orientação
+    angular_velocity = 2.0 * orientation_error
+    
+    # Limitar a velocidade angular a uma faixa razoável
+    angular_velocity = np.clip(angular_velocity, -1.0, 1.0)
+
+    return angular_velocity  # Retorna apenas a velocidade angular
 
 if __name__ == "__main__": 
     rospy.init_node("path_controller_node", anonymous=False)
@@ -22,31 +38,21 @@ if __name__ == "__main__":
     env = Env()
     state_scan = env.reset()
     action = np.zeros(2)
+    min_distance = min(state_scan[:20])
 
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=10)    
     r = rospy.Rate(5) # 10hz
     velocity = Twist()
-    while not rospy.is_shutdown():
-        # Calcular o erro de orientação usando o valor de heading do ambiente
-        orientation_error = env.heading
+while not rospy.is_shutdown():
+    min_distance = min(state_scan[:20])
+    
+    if (min(state_scan[:20])) > 0.5: # Se não tiver obstáculo na frente
+        angular_velocity = alinha_robo(env.heading)
+        action = np.array([0.1, angular_velocity])  # Definir a ação com a velocidade angular
+   
 
-        # Normalizar o erro de orientação para estar no intervalo [-pi, pi]
-        if orientation_error > math.pi:
-            orientation_error -= 2 * math.pi
-        elif orientation_error < -math.pi:
-            orientation_error += 2 * math.pi
-
-        # Calcular a velocidade angular com base no erro de orientação
-        angular_velocity = 2.0 * orientation_error
         
-        # Limitar a velocidade angular a uma faixa razoável
-        angular_velocity = np.clip(angular_velocity, -1.0, 1.0)
-
-        # Definir a ação para controlar apenas a velocidade angular
-        action[0] = 0.0  
-        action[1] = angular_velocity
-        
-        state_scan = env.step(action)
-                
-        r.sleep()
+    state_scan = env.step(action)
+            
+    r.sleep()
 
